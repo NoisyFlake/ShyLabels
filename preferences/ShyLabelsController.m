fb3336f74ba5d62b5c9790b00c042e69b760f937
ConorTheDev <con0r.b@protonmail.com>
2020-02-17 13:51:03 +0000

    Change version to 1.2

--- preferences/ShyLabelsController.m
+++ preferences/ShyLabelsController.m
@@ -81,7 +81,7 @@
         version.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
         version.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:15.0f];
         version.textColor = [UIColor whiteColor];
-        version.text = @"Version 1.1.1";
+        version.text = @"Version 1.2";
         version.backgroundColor = [UIColor clearColor];
         version.textAlignment = NSTextAlignmentCenter;
 
